b1953e90bad3af9bbf9103a49cb05c27cb20de56
Theodore Lytras <thlytras@gmail.com>
2019-11-20 13:48:02 +0200

    Bugfix in labDeaths.R

--- labDeaths.R
+++ labDeaths.R
@@ -385,7 +385,7 @@ if (xlPack=="XLConnect") {
     rb$summSwab <- cbind(nosok = rowSums(mapply(function(x,i){
             a <- loadWorkbook(paste(path_input, "GRIPI_", x, ".xls", sep=""))
             unlist(subset(readWorksheet(a, sheet=i, startRow=3), Week==tgtweek%%100))[-(1:(4-i/2))]
-        }, c("BOREIA", "EKPA", "NOTIA", "KRITI"), c(2,4,2)), na.rm=TRUE),
+        }, c("BOREIA", "EKPA", "NOTIA", "KRITI"), c(2,4,2,4)), na.rm=TRUE),
         sentinel = Reduce(`+`, lapply(c("BOREIA", "NOTIA"), function(x){
             a <- loadWorkbook(paste(path_input, "GRIPI_", x, ".xls", sep=""))
             rowSums(sapply(c(1,3), function(i) {
@@ -400,7 +400,7 @@ if (xlPack=="XLConnect") {
             a <- as.integer(as.character(unlist(a[a[,(4-i/2)]==as.character(tgtweek%%100),])))[-(1:(4-i/2))]
             names(a) <- colnames(all_swabs)
             a
-        }, c("BOREIA", "EKPA", "NOTIA", "KRITI"), c(2,4,2)), na.rm=TRUE),
+        }, c("BOREIA", "EKPA", "NOTIA", "KRITI"), c(2,4,2,4)), na.rm=TRUE),
         sentinel = Reduce(`+`, lapply(c("BOREIA", "NOTIA"), function(x){
             rowSums(sapply(c(1,3), function(i) {
                 a <- read.xls(paste(path_input, "GRIPI_", x, ".xls", sep=""), sheet=i)
@@ -419,7 +419,7 @@ if (xlPack=="XLConnect") {
     rb$summSwabTot <- cbind(nosok = rowSums(mapply(function(x,i){
             a <- loadWorkbook(paste(path_input, "GRIPI_", x, ".xls", sep=""))
             unlist(subset(readWorksheet(a, sheet=i, startRow=3), is.na(Week)))[-(1:(4-i/2))]
-        }, c("BOREIA", "EKPA", "NOTIA", "KRITI"), c(2,4,2)), na.rm=TRUE),
+        }, c("BOREIA", "EKPA", "NOTIA", "KRITI"), c(2,4,2,4)), na.rm=TRUE),
         sentinel = Reduce(`+`, lapply(c("BOREIA", "NOTIA"), function(x){
             a <- loadWorkbook(paste(path_input, "GRIPI_", x, ".xls", sep=""))
             rowSums(sapply(c(1,3), function(i) {
@@ -434,7 +434,7 @@ if (xlPack=="XLConnect") {
             a <- as.integer(as.character(unlist(a[nrow(a),])))[-(1:(4-i/2))]
             names(a) <- colnames(all_swabs)
             a
-        }, c("BOREIA", "EKPA", "NOTIA", "KRITI"), c(2,4,2)), na.rm=TRUE),
+        }, c("BOREIA", "EKPA", "NOTIA", "KRITI"), c(2,4,2,4)), na.rm=TRUE),
         sentinel = Reduce(`+`, lapply(c("BOREIA", "NOTIA"), function(x){
             rowSums(sapply(c(1,3), function(i) {
                 a <- read.xls(paste(path_input, "GRIPI_", x, ".xls", sep=""), sheet=i)
